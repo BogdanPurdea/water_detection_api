2c24800a5c969000b5eda1a7b7a5a65d22e0e7a1
Bogdan Purdea <bogdanpurdea14@gmail.com>
2025-03-25 23:29:22 +0200

    Limit the number of images retrieved through GEE

--- api/modules/data_retrievers.py
+++ api/modules/data_retrievers.py
@@ -5,8 +5,9 @@ def get_sentinel2_collection(roi, start_date, end_date):
     return ee.ImageCollection('COPERNICUS/S2_HARMONIZED') \
         .filterBounds(roi) \
         .filterDate(start_date, end_date) \
-        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10))
-
+        .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10)) \
+        .limit(10)
+        
 def get_sentinel1_collection(roi, start_date, end_date):
     """Retrieve Sentinel-1 imagery for the given parameters."""
     return ee.ImageCollection('COPERNICUS/S1_GRD') \
